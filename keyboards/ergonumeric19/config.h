8700efde632ddefa6cd42ebbe25994975b5ea1a6
underMHz <142560868+underMHz@users.noreply.github.com>
2023-12-03 19:13:06 +0900

    Update config.h

--- keyboards/ergonumeric19/config.h
+++ keyboards/ergonumeric19/config.h
@@ -1,19 +1,18 @@
-// Copyright 2023 underMHz (@underMHz)
-// SPDX-License-Identifier: GPL-2.0-or-later
 
 #pragma once
 
-#define VENDOR_ID       0x554D
-#define PRODUCT_ID      0x0001
-#define DEVICE_VER      0x0001
-#define MANUFACTURER    underMHz
-#define PRODUCT         ergonumeric19
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
 
-/* key matrix size */
-#define MATRIX_ROWS 5
-#define MATRIX_COLS 4
+/* disable debug print */
+//#define NO_DEBUG
 
-// wiring of each half
-#define MATRIX_ROW_PINS { D4, C6, D7, E6, B4 }
-#define MATRIX_COL_PINS { B6, B2, B3, B1 }
-#define UNUSED_PINS
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
